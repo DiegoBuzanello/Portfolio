a1220b68371e67113997472601232530a0ec4eb6
DiegoBuzanello - Desktop <buzanello.diego@gmail.com>
2020-08-08 23:14:11 -0300

    Updates

---
+++ precache-manifest.a63427b54f47903a5fbf30415d66aefb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe917d48337f2d7eb606d0ce3b3cca6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "07d222472d4c79159a68",
+    "url": "/portfolio/static/css/main.a29acd96.chunk.css"
+  },
+  {
+    "revision": "f9e3ede3f473fc845b95",
+    "url": "/portfolio/static/js/2.942482da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.942482da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07d222472d4c79159a68",
+    "url": "/portfolio/static/js/main.3482e9a2.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "3ea000b5081ac831bb4828ba41a1afd7",
+    "url": "/portfolio/static/media/ecoleta.3ea000b5.png"
+  },
+  {
+    "revision": "4e7c409a7f3693f290e894c5edb36f5b",
+    "url": "/portfolio/static/media/hibike.4e7c409a.png"
+  },
+  {
+    "revision": "35a03e760681c85f17a29d54398ef4ad",
+    "url": "/portfolio/static/media/planob.35a03e76.png"
+  },
+  {
+    "revision": "daee8c83b5dd3b2763778368ae287295",
+    "url": "/portfolio/static/media/proffy.daee8c83.png"
+  },
+  {
+    "revision": "1591567618c5872aea1673968af6f3f1",
+    "url": "/portfolio/static/media/punkpaulista.15915676.png"
+  }
+]);
